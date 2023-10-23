a8581690ccb5ac25735fbdd9d6fa5adcb99de44d
xmxwl <148741711+xmxwl@users.noreply.github.com>
2023-10-23 21:11:23 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -145,10 +145,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT PRO
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          THIS IS PRO.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
